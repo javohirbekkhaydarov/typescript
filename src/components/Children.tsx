e09a3ef57b407bd5d899bc9c412265424deefdb2
Javohirbek <javohirbekkhaydarov@gmail.com>
2022-10-19 22:28:10 +0500

    typescript start

---
+++ src/components/Children.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Children = () => {
+  return (
+    <div>Children</div>
+  )
+}
+
+export const Children2 = () => {
+  return (
+    <div>
+      this is children2
+    </div>
+  )
+}
